3ab6f76765f9d2164a3583e0ce48a53a0523b306
easytigers <easytigers176@gmail.com>
2022-05-30 23:30:31 -0400

    SQL added

--- TechJobsPersistent/queries.sql
+++ TechJobsPersistent/queries.sql
@@ -1,6 +1,18 @@
 --Part 1
 
+SELECT column_name, data_type
+FROM information_schema.columns
+WHERE table_name = "Jobs"
+
 --Part 2
+
+SELECT Name 
+FROM employers
+WHERE Location = "St. Louis";
 
 --Part 3
 
+SELECT Name, Description 
+FROM jobSkills
+WHERE techjobs.jobSkills.jobId IS NOT NULL
+ORDER BY Name ASC;
